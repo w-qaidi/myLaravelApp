f75545dbb822d6fbbb6fad51fbb21402ce11bafe
Wassim Qaidi <wassim.qaidi@gmail.com>
2022-01-04 00:17:53 -0500

    gitingore

---
+++ vendor/jeroennoten/laravel-adminlte/resources/views/partials/sidebar/menu-item.blade.php
@@ -0,0 +1,23 @@
+@inject('sidebarItemHelper', 'JeroenNoten\LaravelAdminLte\Helpers\SidebarItemHelper')
+
+@if ($sidebarItemHelper->isHeader($item))
+
+    {{-- Header --}}
+    @include('adminlte::partials.sidebar.menu-item-header')
+
+@elseif ($sidebarItemHelper->isLegacySearch($item) || $sidebarItemHelper->isCustomSearch($item))
+
+    {{-- Search form --}}
+    @include('adminlte::partials.sidebar.menu-item-search-form')
+
+@elseif ($sidebarItemHelper->isMenuSearch($item))
+
+    {{-- Search menu --}}
+    @include('adminlte::partials.sidebar.menu-item-search-menu')
+
+@elseif ($sidebarItemHelper->isLink($item))
+
+    {{-- Link --}}
+    @include('adminlte::partials.sidebar.menu-item-link')
+
+@endif
